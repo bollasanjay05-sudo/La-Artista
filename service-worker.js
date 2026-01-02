aacb878726602a528cab58e826a38fd75c55848b
bollasanjay05-sudo <bollasanjay05@gmail.com>
2026-01-02 14:53:32 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,68 +1,23 @@
-const CACHE_NAME = 'lartista-v1';
+const CACHE_NAME = 'lartista-v2';
 const urlsToCache = [
     '/',
     '/index.html',
     '/css/style.css',
     '/js/script.js',
     '/manifest.json',
-    '/images/lartista-logo.png'
+    '/images/your-logo.png'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-            })
-            .catch(err => console.log('Cache addAll error:', err))
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
-                    .then(response => {
-                        // Check if we received a valid response
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        
-                        // Clone the response
-                        const responseToCache = response.clone();
-                        
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-                        
-                        return response;
-                    });
-            })
-            .catch(() => {
-                // If both cache and network fail, show offline page
-                return caches.match('/offline.html');
-            })
+            .then(response => response || fetch(event.request))
     );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+});5
